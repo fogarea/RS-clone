6f8ee0fbf445e8a55c7b8c232d250ec50e9aeee1
Elijah <elijah.ivanik@inbox.ru>
2023-02-27 08:55:32 +0300

    fix: 404 html

--- src/404.ts
+++ src/404.ts
@@ -4,12 +4,19 @@ import { Routing } from "types/route.types";
 import navigationModel from "model/navigation.model";
 import { state } from "store/state";
 
-if (Object.values(Routing).includes(navigationModel.route.path as Routing)) {
+if (
+  Object.values(Routing).includes(navigationModel.route.resource as Routing)
+) {
   localStorage.setItem("restore.route", navigationModel.route.href);
   const win: Window = window;
   win.location = navigationModel.route.origin + state.basePath;
 } else {
+  document.body.setAttribute(
+    "style",
+    "width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; overflow: hidden;"
+  );
   const img404 = document.createElement("img");
+  img404.setAttribute("style", "max-width: 90vw; max-height: 90vh;");
   img404.src = img404Src;
 
   document.body.append(img404);
